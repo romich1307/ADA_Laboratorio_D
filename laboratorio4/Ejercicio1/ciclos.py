9d8de4c1b4a1fe6d60167205ad131f935b76a792
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-10-06 12:59:18 -0500

    Se realiza el metodo union, para unir

--- laboratorio4/Ejercicio1/ciclos.py
+++ laboratorio4/Ejercicio1/ciclos.py
@@ -12,4 +12,14 @@ class ciclos:
         if objeto.padre[i] !=i:
             objeto.padre[i]=objeto.encontar(objeto.padre[i]) 
         return objeto.padre[i]
-        
+    def union(objeto,nodo_u,nodo_v):
+        raiz_u=objeto.encontrar(nodo_u)
+        raiz_v=objeto.encontrar(nodo_v)
+        if raiz_u !=raiz_v:
+            if objeto.rango[raiz_u]>objeto.rango[raiz_v]:
+                objeto.padre[raiz_v]=raiz_u
+            elif objeto.rango[raiz_u]<objeto.rango[raiz_v]:
+                objeto.padre[raiz_u]=raiz_v
+            else:
+                objeto.padre[raiz_v]=raiz_u
+                objeto.rango[raiz_u]+=1
